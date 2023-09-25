4afd37ddc8cdb94813b3526f2f4d8a234d54a8c4
garghar06 <145961408+garghar06@users.noreply.github.com>
2023-09-25 12:13:43 +0530

    Create circle.py

---
+++ circle.py
@@ -0,0 +1,16 @@
+class Circle():
+    def __init__(self,r):
+        self.radius=r
+    
+    def area(Self):
+        areaOfCircle=3.14*Self.radius**2
+        return areaOfCircle
+    
+    def perimeter(Self):
+        peri=2*Self.radius*3.14
+        return peri
+    
+NewCircle=Circle(4)
+print(NewCircle.area())
+print(NewCircle.perimeter())
+
